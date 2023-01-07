92eb0c625dcb4da9c359baa4508655b88173d287
Ben Weber <benweberswe@gmail.com>
2023-01-07 17:31:39 -0600

    updates for movement, highlighting, and move detection

--- src/main/java/com/example/chessapp/Piece.java
+++ src/main/java/com/example/chessapp/Piece.java
@@ -52,7 +52,7 @@ public class Piece extends ImageView {
         });
 
         this.setOnMouseDragged(mouseEvent -> {
-            getPossibleMoves();
+            getPossibleMoves();s
             highlightMoves(possibleMoves);
             this.setTranslateX(mouseEvent.getSceneX() - startDragX);
             this.setTranslateY(mouseEvent.getSceneY() - startDragY);
