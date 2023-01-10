fa3468413e4821082fb22aacda0d4ddf947ff18e
Ben Weber <benweberswe@gmail.com>
2023-01-10 11:04:12 -0600

    Move pieces drag and drop feature

--- src/main/java/com/example/chessapp/Piece.java
+++ src/main/java/com/example/chessapp/Piece.java
@@ -82,7 +82,7 @@ public class Piece extends ImageView {
         Color highlightColor = Color.rgb(144, 238, 144);
         highLightedSquares = new ArrayList<>();
         if(possibleMoves.isEmpty()){
-            return;ssss
+            return;sssss
         }
 
         for(Square square : squares){
